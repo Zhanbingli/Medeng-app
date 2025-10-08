c0ca53226237400aa458c6a8e39bfd6dfebc60cd
zhanbingli <3163525375@qq.com>
2025-10-08 20:04:47 +0800

    Initial Commit

---
+++ Medeng/MedengApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedengApp.swift
+//  Medeng
+//
+//  Created by lizhanbing12 on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MedengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MedengTests/MedengTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedengTests.swift
+//  MedengTests
+//
+//  Created by lizhanbing12 on 8/10/25.
+//
+
+import Testing
+@testable import Medeng
+
+struct MedengTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
